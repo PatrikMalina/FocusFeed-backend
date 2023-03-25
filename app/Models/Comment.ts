6cb469a0f02d38a155385ae16a722720ab2685a9
Patrik Malina <patrik.malina.jr@gmail.com>
2023-03-25 16:50:38 +0100

    Added comment migration and model

--- app/Models/Comment.ts
+++ app/Models/Comment.ts
@@ -1,9 +1,17 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, hasOne, HasOne } from '@ioc:Adonis/Lucid/Orm'
+import Post from './Post'
+import User from './User'
 
 export default class Comment extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @hasOne(() => Post)
+  public postId: HasOne<typeof Post>
+
+  @hasOne(() => User)
+  public userId: HasOne<typeof User>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
