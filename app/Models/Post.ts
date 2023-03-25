da93d862c2f7962f5311089b05a90c6f753aa822
Patrik Malina <patrik.malina.jr@gmail.com>
2023-03-25 16:49:16 +0100

    Added comment migration and model

--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -2,6 +2,7 @@ import { DateTime } from 'luxon'
 import { BaseModel, column, HasMany, hasMany, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
 import Like from './Like'
+import Comment from './Comment'
 
 export default class Post extends BaseModel {
   @column({ isPrimary: true })
@@ -23,6 +24,11 @@ export default class Post extends BaseModel {
     foreignKey: 'postId',
   })
   public likes: HasMany<typeof Like>
+
+  @hasMany(() => Comment, {
+    foreignKey: 'postId',
+  })
+  public comments: HasMany<typeof Comment>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
