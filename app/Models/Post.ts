040c58efbfdc4e27f93c489950c71b4b04c89167
Patrik Malina <patrik.malina.jr@gmail.com>
2023-03-25 16:46:44 +0100

    Edited post model

--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
+import Like from './Like'
 
 export default class Post extends BaseModel {
   @column({ isPrimary: true })
@@ -8,6 +9,20 @@ export default class Post extends BaseModel {
 
   @hasOne(() => User)
   public createdBy: HasOne<typeof User>
+
+  @column()
+  public location: number[]
+
+  @column()
+  public caption: string
+
+  @column()
+  public pictureUrl: string
+
+  @hasMany(() => Like, {
+    foreignKey: 'postId',
+  })
+  public likes: HasMany<typeof Like>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
